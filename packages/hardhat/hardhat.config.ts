9c87170c6c8d7bb93e8b1d6f65bcb190249854e0
Zishan <amanzishan.az@gmail.com>
2021-10-08 16:04:17 +0530

    debugged fetch graph data

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,9 +1,9 @@
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-waffle';
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
-import '@typechain/hardhat';
-import { task } from 'hardhat/config';
-import { HardhatUserConfig } from 'hardhat/types';
+import "@typechain/hardhat";
+import { task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/types";
 
 const accounts = {
   mnemonic:
@@ -12,7 +12,7 @@ const accounts = {
 };
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (_args, hre) => {
+task("accounts", "Prints the list of accounts", async (_args, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -27,9 +27,9 @@ task('accounts', 'Prints the list of accounts', async (_args, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 const config: HardhatUserConfig = {
-  solidity: '0.8.3',
+  solidity: "0.8.3",
   paths: {
-    artifacts: '../frontend/artifacts',
+    artifacts: "../frontend/artifacts",
   },
   networks: {
     hardhat: {
@@ -38,21 +38,21 @@ const config: HardhatUserConfig = {
       forking: {
         enabled: true,
         url: `https://eth-kovan.alchemyapi.io/v2/HI8npM_DUD5_7FM24hWts9cN_73kDRg3`,
-        blockNumber: 27534667
-      }
+        blockNumber: 27534667,
+      },
     },
     kovan: {
-      url: "https://eth-kovan.alchemyapi.io/v2/HI8npM_DUD5_7FM24hWts9cN_73kDRg3", 
+      url: "https://eth-kovan.alchemyapi.io/v2/HI8npM_DUD5_7FM24hWts9cN_73kDRg3",
       accounts,
     },
   },
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_KEY
-  },
+  // etherscan: {
+  //   // Your API key for Etherscan
+  //   // Obtain one at https://etherscan.io/
+  //   apiKey: process.env.ETHERSCAN_KEY
+  // },
   typechain: {
-    outDir: '../frontend/types/typechain',
+    outDir: "../frontend/types/typechain",
   },
 };
 
--- packages/hardhat/scripts/superfluid.ts
+++ packages/hardhat/scripts/superfluid.ts
@@ -1,10 +1,11 @@
-const SuperfluidSDK = require("@superfluid-finance/js-sdk");
-const { Web3Provider, InfuraProvider } = require("@ethersproject/providers");
-const {ethers} = require("ethers");
-
+import SuperfluidSDK from "@superfluid-finance/js-sdk";
+import { Web3Provider, InfuraProvider } from "@ethersproject/providers";
+import { ethers } from "ethers";
+import fetchactiveProposals from "../api"
 let privateKey = "";
 let wallet = new ethers.Wallet(privateKey);
-
+let activeProposals = await fetchactiveProposals
+console.log(activeProposals)
 // Connect a wallet to mainnet
 let provider = new InfuraProvider("goerli", {
   projectId: "9f3370d63f484b24a73fc28c6b487ee4",
@@ -29,61 +30,19 @@ console.log("Project's - ",details);
 
 //Graphql queries and calls
   
-const fetch = require("node-fetch");
-const GRAPHQL_URL = "https://hub.snapshot.org/graphql";
-async function fetchProposalData() {
-  const response = await fetch(GRAPHQL_URL, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      query: 
-      query {
-        proposal(id:"QmWbpCtwdLzxuLKnMW4Vv4MPFd2pdPX71YBKPasfZxqLUS") {
-          id
-          title
-          body
-          choices
-          start
-          end
-          snapshot
-          state
-          author
-          created
-          plugins
-          network
-          strategies {
-            name
-            params
-          }
-          space {
-            id
-            name
-          }
-        }
-      }
-      ,
-    }),
-  });
 
-  const responseBody = await response.json();
-  console.log(responseBody);
-  let addressdata = responseBody["data"]["body"];
-  return addressdata;
-}
 
 
 //This is the IDA portion
 
 let x=0;
-let recipientaddresses[]=fetchProposalData()["body"]["address"];
-let recipientshares[]=fetchProposalData()["body"]["percentages"];
+let recipientaddresses[]=activeProposals["body"]["address"];
+let recipientshares[]=activeProposals["body"]["percentages"];
 
 await project.createPool({ poolId: 1 });
 await project.giveShares({ poolId: 1, recipient: recipientaddresses[x], shares: recipientshares[x] });
 x++;
-await project.giveShares({ poolId: 1, recipient: recipientadresses[x], shares: recipientshares[x] });
+await project.giveShares({ poolId: 1, recipient: recipientaddresses[x], shares: recipientshares[x] });
 
 await project.distributeToPool({ poolId: 1, amount: 1000 });
 
